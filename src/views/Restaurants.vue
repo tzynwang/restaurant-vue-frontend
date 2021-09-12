23ebd6de9062bba7ac44fbe5bf53832f530d05e4
tzyn (mac) <tzyn.wang@gmail.com>
2021-09-12 13:32:44 +0800

    feat: add components Restaurants, RestaurantsFeeds, RestaurantsTop, UsersTop

---
+++ src/views/Restaurants.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container py-5">
+    <h1 class="mt-5">首頁 - 餐廳列表</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Restaurants",
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/RestaurantsTop.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container py-5">
+    <h1 class="mt-5">人氣餐廳</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RestaurantsTop",
+};
+</script>
+
+<style>
+</style>
