139573b446605c4efa568b68a911282687be1c55
tzyn (mac) <tzyn.wang@gmail.com>
2021-09-12 13:24:50 +0800

    feat: add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="restaurant-forum">
-    <router-view/>
+    <Navbar />
+    <main role="main" class="mt-5 bg-white">
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+import Navbar from "./components/Navbar";
 
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
 </script>
 
 <style>
