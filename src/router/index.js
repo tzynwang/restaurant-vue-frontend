5b6fc45ec20a985542089a1b9227ec109448e57e
tzyn (mac) <tzyn.wang@gmail.com>
2021-09-12 13:31:52 +0800

    feat: add routes root, restaurants, restaurants-feeds, restaurants-tops, users-top

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: { name: 'restaurants' }
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -14,6 +21,26 @@ const routes = [
     path: '/signup',
     name: 'sign-up',
     component: () => import('../views/SignUp.vue')
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds.vue')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-tops',
+    component: () => import('../views/RestaurantsTop.vue')
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop.vue')
   },
   {
     path: '*',
