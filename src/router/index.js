a015106f6b8f968c2cd5042d0e6d425169131834
tzyn (mac) <tzyn.wang@gmail.com>
2021-09-12 13:07:03 +0800

    feat: add SignIn

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import SignIn from '../views/SignIn.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
     path: '*',
-    name: 'NotFound',
+    name: 'not-found',
     component: () => import('../views/NotFound.vue')
   }
 ]
