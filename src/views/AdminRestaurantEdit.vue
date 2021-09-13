eadead6a4c3579370c16f8781fc9a70d50eb3f28
tzyn.wang <tzyn.wang@gmail.com>
2021-09-13 13:05:58 +0800

    feat: add AdminRestaurantEdit

---
+++ src/views/AdminRestaurantEdit.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container py-5">
+    <AdminRestaurantForm
+      :initial-restaurant="restaurant"
+      @after-submit="handleAfterSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from "./../components/AdminRestaurantForm.vue";
+
+const dummyData = {
+  restaurant: {
+    id: 1,
+    name: "Laurence Reynolds",
+    tel: "1-657-067-3756 x9782",
+    address: "187 Kirlin Squares",
+    opening_hours: "08:00",
+    description: "sit est mollitia",
+    image:
+      "https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887",
+    viewCounts: 1,
+    createdAt: "2019-07-30T16:24:55.432Z",
+    updatedAt: "2019-07-30T17:26:43.260Z",
+    CategoryId: 3,
+    Category: {
+      id: 3,
+      name: "義大利料理",
+      createdAt: "2019-07-30T16:24:55.429Z",
+      updatedAt: "2019-07-30T16:24:55.429Z",
+    },
+  },
+};
+
+export default {
+  name: "AdminRestaurantEdit",
+  components: {
+    AdminRestaurantForm,
+  },
+  data() {
+    return {
+      restaurant: {
+        id: -1,
+        name: "",
+        categoryId: "",
+        tel: "",
+        address: "",
+        description: "",
+        image: "",
+        openingHours: "",
+      },
+    };
+  },
+  created() {
+    // 透過 this.$route.params 取得網址上的 id，才知道現在在編輯哪一個餐廳
+    const { id } = this.$route.params;
+    this.fetchRestaurant(id);
+  },
+  methods: {
+    fetchRestaurant(restaurantId) {
+      const { restaurant } = dummyData;
+      this.restaurant = {
+        ...this.restaurant,
+        id: restaurant.id,
+        name: restaurant.name,
+        categoryId: restaurant.CategoryId,
+        tel: restaurant.tel,
+        address: restaurant.address,
+        description: restaurant.description,
+        image: restaurant.image,
+        openingHours: restaurant.opening_hours,
+      };
+    },
+    handleAfterSubmit(payload) {
+      console.log(`console.log from AdminRestaurantEdit: ${payload}`);
+    },
+  },
+};
+</script>
