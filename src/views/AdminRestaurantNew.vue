9dafca671140d1945a870ed41f3c077973a8d7d3
tzyn.wang <tzyn.wang@gmail.com>
2021-09-13 12:48:04 +0800

    feat: add AdminRestaurantNew

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container py-5">
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from "./../components/AdminRestaurantForm";
+
+export default {
+  name: "AdminRestaurantNew",
+  components: {
+    AdminRestaurantForm,
+  },
+  methods: {
+    handleAfterSubmit (payload) {
+      console.log(`console.log from AdminRestaurantNew: ${payload}`)
+    }
+  }
+};
+</script>
