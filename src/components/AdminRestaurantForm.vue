471b62fcc8a457848d380e3722baedc49661d347
tzyn.wang <tzyn.wang@gmail.com>
2021-09-13 12:48:20 +0800

    feat: add component AdminRestaurantForm

---
+++ src/components/AdminRestaurantForm.vue
@@ -0,0 +1,182 @@
+<template>
+  <form @submit.stop.prevent="handleSubmit">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input
+        id="name"
+        v-model="restaurant.name"
+        type="text"
+        class="form-control"
+        name="name"
+        placeholder="Enter name"
+        required
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="categoryId">Category</label>
+      <select
+        id="categoryId"
+        v-model="restaurant.categoryId"
+        class="form-control"
+        name="categoryId"
+        required
+      >
+        <option value="" selected disabled>
+          --請選擇--
+        </option>
+        <option
+          v-for="category in categories"
+          :key="category.id"
+          :value="category.id"
+        >
+          {{ category.name }}
+        </option>
+      </select>
+    </div>
+
+    <div class="form-group">
+      <label for="tel">Tel</label>
+      <input
+        id="tel"
+        v-model="restaurant.tel"
+        type="text"
+        class="form-control"
+        name="tel"
+        placeholder="Enter telephone number"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="address">Address</label>
+      <input
+        id="address"
+        v-model="restaurant.address"
+        type="text"
+        class="form-control"
+        placeholder="Enter address"
+        name="address"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="opening-hours">Opening Hours</label>
+      <input
+        id="opening-hours"
+        v-model="restaurant.openingHours"
+        type="time"
+        class="form-control"
+        name="opening_hours"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="description">Description</label>
+      <textarea
+        id="description"
+        v-model="restaurant.description"
+        class="form-control"
+        rows="3"
+        name="description"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="image">Image</label>
+      <img
+        v-if="restaurant.image"
+        :src="restaurant.image"
+        class="d-block img-thumbnail mb-3"
+        width="200"
+        height="200"
+        style="object-fit: cover;"
+      />
+      <input
+        id="image"
+        type="file"
+        name="image"
+        accept="image/*"
+        class="form-control-file"
+        @change="handleFileChange"
+      />
+    </div>
+
+    <button type="submit" class="btn btn-primary">
+      送出
+    </button>
+  </form>
+</template>
+
+<script>
+const dummyData = {
+  categories: [
+    {
+      id: 1,
+      name: "中式料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z",
+    },
+    {
+      id: 2,
+      name: "日本料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z",
+    },
+    {
+      id: 3,
+      name: "義大利料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z",
+    },
+    {
+      id: 4,
+      name: "墨西哥料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z",
+    },
+  ],
+};
+
+export default {
+  name: "AdminRestaurantForm",
+  data() {
+    return {
+      restaurant: {
+        name: "",
+        categoryId: "",
+        tel: "",
+        address: "",
+        description: "",
+        image: "",
+        openingHours: "",
+      },
+      categories: [],
+    };
+  },
+  created() {
+    this.fetchCategories();
+  },
+  methods: {
+    fetchCategories() {
+      this.categories = dummyData.categories;
+    },
+    handleFileChange(event) {
+      const { files } = event.target;
+      if (files.length) {
+        // 有上傳檔案，產生預覽圖
+        const imageURL = window.URL.createObjectURL(files[0]);
+        this.restaurant.image = imageURL;
+      } else {
+        // 使用者沒有選擇上傳的檔案
+        this.restaurant.image = "";
+      }
+    },
+    handleSubmit(event) {
+      const form = event.target;
+      const formData = new FormData(form);
+      // 把資料傳給親元件
+      this.$emit('after-submit', formData)
+    },
+  },
+};
+</script>
