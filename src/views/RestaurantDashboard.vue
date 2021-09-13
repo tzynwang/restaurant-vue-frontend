443f694f75c7d90b924884e2bb72158615d0ad91
tzyn.wang <tzyn.wang@gmail.com>
2021-09-13 23:10:44 +0800

    feat: RestaurantDashboard fetch real data from API

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -21,89 +21,8 @@
 </template>
 
 <script>
-const dummyData = {
-  restaurant: {
-    id: 41,
-    name: "Annie Collins",
-    tel: "076.299.6499",
-    address: "237 Ashlynn Flats",
-    opening_hours: "08:00",
-    description:
-      "Provident enim ipsa iste id minima quia explicabo. Odit maxime voluptatem est recusandae aut. Ad dolorum aliquam minima iste aut.",
-    image:
-      "https://loremflickr.com/320/240/restaurant,food/?random=37.081383081285054",
-    viewCounts: 0,
-    createdAt: "2021-09-08T08:34:40.000Z",
-    updatedAt: "2021-09-08T08:34:40.000Z",
-    CategoryId: 1,
-    Category: {
-      id: 1,
-      name: "中式料理",
-      createdAt: "2021-09-08T08:34:40.000Z",
-      updatedAt: "2021-09-08T08:34:40.000Z",
-    },
-    Comments: [
-      {
-        id: 91,
-        text: "Accusantium praesentium voluptates hic labore eaque.",
-        UserId: 1,
-        RestaurantId: 41,
-        createdAt: "2021-09-08T08:34:40.000Z",
-        updatedAt: "2021-09-08T08:34:40.000Z",
-        User: {
-          id: 1,
-          name: "root",
-          email: "root@example.com",
-          password:
-            "$2a$10$PM9K3ZslTQWzH3zTNX7JqOD9m5SmUR5eNfDRx6TV2Ujrbgr.7ie.a",
-          isAdmin: true,
-          image: null,
-          createdAt: "2021-09-08T08:34:40.000Z",
-          updatedAt: "2021-09-08T08:34:40.000Z",
-        },
-      },
-      {
-        id: 41,
-        text: "Ut voluptas est dolorem rem.",
-        UserId: 3,
-        RestaurantId: 41,
-        createdAt: "2021-09-08T08:34:40.000Z",
-        updatedAt: "2021-09-08T08:34:40.000Z",
-        User: {
-          id: 3,
-          name: "user2",
-          email: "user2@example.com",
-          password:
-            "$2a$10$6VAzzV4YdId/.jquKOOC8e32tdc6k/VgZfJPCCxwgMl/xAe39UXXW",
-          isAdmin: false,
-          image: null,
-          createdAt: "2021-09-08T08:34:40.000Z",
-          updatedAt: "2021-09-08T08:34:40.000Z",
-        },
-      },
-      {
-        id: 141,
-        text:
-          "Alias praesentium corrupti non cum nostrum corporis voluptatibus adipisci.",
-        UserId: 3,
-        RestaurantId: 41,
-        createdAt: "2021-09-08T08:34:40.000Z",
-        updatedAt: "2021-09-08T08:34:40.000Z",
-        User: {
-          id: 3,
-          name: "user2",
-          email: "user2@example.com",
-          password:
-            "$2a$10$6VAzzV4YdId/.jquKOOC8e32tdc6k/VgZfJPCCxwgMl/xAe39UXXW",
-          isAdmin: false,
-          image: null,
-          createdAt: "2021-09-08T08:34:40.000Z",
-          updatedAt: "2021-09-08T08:34:40.000Z",
-        },
-      },
-    ],
-  },
-};
+import restaurantsAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/helpers";
 
 export default {
   name: "RestaurantDashboard",
@@ -119,19 +38,39 @@ export default {
     };
   },
   created() {
-    this.fetchRestaurant();
+    const { id } = this.$route.params;
+    this.fetchRestaurant(id);
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { id } = to.params;
+    this.fetchRestaurant(id);
+    next();
   },
   methods: {
-    fetchRestaurant() {
-      const { id, name, Category, Comments, viewCounts } = dummyData.restaurant;
-      this.restaurant = {
-        ...this.restaurant,
-        id,
-        name,
-        categoryName: Category ? Category.name : "未分類",
-        commentsLength: Comments.length,
-        viewCounts,
-      };
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await restaurantsAPI.getRestaurant({ restaurantId });
+
+        if (data.status === "error") {
+          throw new Error(data.message);
+        }
+
+        const { id, name, Category, Comments, viewCounts } = data.restaurant;
+
+        this.restaurant = {
+          ...this.restaurant,
+          id,
+          name,
+          categoryName: Category ? Category.name : "未分類",
+          commentsLength: Comments.length,
+          viewCounts,
+        };
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: "無法取得餐廳資料，請稍後再試",
+        });
+      }
     },
   },
 };
