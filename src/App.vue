2f31d0cf94749a2d9b1614e46c7009f704017751
tzyn.wang <tzyn.wang@gmail.com>
2021-09-15 11:13:31 +0800

    feat: add custom style

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,8 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+main {
+  margin-top: 65px !important;
+}
 </style>
--- src/components/RestaurantComments.vue
+++ src/components/RestaurantComments.vue
@@ -32,7 +32,7 @@
 <script>
 import { fromNowFilter } from "./../utils/mixins";
 import { mapState } from "vuex";
-import commentsAPI from './../apis/comments'
+import commentsAPI from "./../apis/comments";
 import { Toast } from "./../utils/helpers";
 
 export default {
@@ -73,3 +73,19 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+h2.my-4 {
+  margin-bottom: 1rem !important;
+  font-size: 18px;
+}
+
+h3 {
+  margin-bottom: 3px;
+  line-height: 1.3;
+}
+
+.blockquote-footer {
+  font-size: 12.5px;
+}
+</style>
